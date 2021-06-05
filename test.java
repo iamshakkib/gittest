560810b4c07c3824dac6097b10681d452f4332af
shakkib327559 <md2076379@gmail.com>
2021-06-05 13:19:25 +0530

    2nd commit

---
+++ test.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class test{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+			System.out.println("hello new integration in the domain of competitive programming");
+		}
+	}
+}
